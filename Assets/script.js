4cb55416a14b95c6e040d5eb019d1b2cf87ef770
ivanapina <ivanapina@gmail.com>
2023-05-18 22:06:58 -0500

    Added final code and finished README file

--- Assets/script.js
+++ Assets/script.js
@@ -6,50 +6,49 @@ function generatePassword() {
 
 // Array for every password criteria
 
-var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numbArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var characterArray = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+var uppercaseChoice = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercaseChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numberChoice = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChoice = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
 
-// empty array
  
 var resultArray = [];
 var userArray = [];
 
-uppercaseArray [1]
+// Setting our browser window questions to meet the criteria
 
-var numCharacter = prompt ("You must choose at least 8 characters and no more than 128 characters");
-var numbers = confirm ("Do you want numbers in your password?");
-var uppercases = confirm ("Do you want uppercases in your password?");
-var lowercases = confirm ("Do want lowercases in your password?");
-var characters = confirm ("Do you want special characters in your password?");
+var totalCharacter = window.prompt ("Choose between 8 characters and 128 characters for your password");
+var numbers = window.confirm ("Do you want numbers in your password?");
+var uppercases = window.confirm ("Do you want uppercases in your password?");
+var lowercases = window.confirm ("Do want lowercases in your password?");
+var specialCharacters = window.confirm ("Do you want special characters in your password?");
 
 
 // If statements for the conditions
 
 if (numbers){
-  resultArray = resultArray.concat(numbArray);
+  resultArray = resultArray.concat(numberChoice);
   
 }
 
 if (uppercases){
-  resultArray = resultArray.concat(uppercaseArray);
+  resultArray = resultArray.concat(uppercaseChoice);
 
 }
 
 if (lowercases){
-  resultArray = resultArray.concat(lowercaseArray);
+  resultArray = resultArray.concat(lowercaseChoice);
 
 }
 
-if (characters){
-  resultArray = resultArray.concat(characterArray);
+if (specialCharacters){
+  resultArray = resultArray.concat(specialChoice);
 }
 
 console.log(resultArray)
 
 
-for (var i = 0; i < numCharacter; i++) {
+for (var i = 0; i < totalCharacter; i++) {
       
   userArray.push (resultArray[Math.floor(Math.random() * resultArray.length)]); 
   }
